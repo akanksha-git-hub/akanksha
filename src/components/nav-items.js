ae4ae52482b9febebc80e54ef7b7918efe6cfca7
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-10-23 11:58:35 +0530

    nav bar bug fix duplicate header values

--- src/components/nav-items.js
+++ src/components/nav-items.js
@@ -4,62 +4,89 @@ import RichText from "./Texts/RichText";
 
 export default function NavItems({ header_link_items, drop_down_items }) {
 
-    return(
-        <>
-        {header_link_items.length > 0 && (
-            <ul className="hidden lg:flex items-center gap-8">
-              {header_link_items.map(item => {
+  const uniqueIdentifier = [...new Set(drop_down_items.map(item => {
+    const originalCaseValue = item.identifier;
+    const lowerCaseValue = originalCaseValue.toLowerCase();
+    return lowerCaseValue;
+  }))];
 
-                if(item.dropdown) {
+  return(
+      <>
+      {header_link_items.length > 0 && (
+          <ul className="hidden lg:flex items-center gap-8">
+            {header_link_items.map(item => {
 
-                  const uniqueIdentifier = [...new Set(drop_down_items.map(item => {
-                    const originalCaseValue = item.identifier;
-                    return originalCaseValue
-                  }))];
+              if(item.dropdown) {
 
-                  return(
-                      uniqueIdentifier.map((item, index) => (
+                // const uniqueIdentifier = [...new Set(drop_down_items.map(item => {
+                //   const originalCaseValue = item.identifier;
+                //   return originalCaseValue
+                // }))];
+
+                return(
+                    uniqueIdentifier.map((item, index) => {
+
+                      const upper = item.split(" ");
+                      const upperCaseValue = upper.map((item, i) => {
+
+                        const firstLetter = item[0].toUpperCase();
+                        const otherLetters = item.substring(1);
+                        const finalWord = `${firstLetter}${otherLetters}`;
+                        return finalWord;
                         
-                        <li key={index} className="relative">
-                          <div className="drop-down">
-                            <RichText 
-                              text={item}
-                              className="text-deep-green text-base font-inter hover:opacity-55 transition-all active:scale-95 relative cursor-pointer"
-                            />
-                            <div className="py-4 space-y-4 z-20 bg-cream border border-deep-green rounded min-w-[200px] drop-down-container">
-                              {drop_down_items
-                                .filter((data) => {
-                                    const lowerCaseValue = item.toLowerCase();
-                                    const matchData = lowerCaseValue === data.identifier.toLowerCase();
-                                    return matchData;
-                                })
-                                .map((drop_down, i) => {
-                                  return(
-                                    <p className="px-4 hover:opacity-55 transition-all active:scale-95" key={i}>
-                                      <PrismicNextLink
-                                        field={drop_down.cta_link}>
-                                        <span className="text-deep-green text-base font-inter">
-                                          {drop_down.cta_text}
-                                        </span>
-                                      </PrismicNextLink>
-                                    </p>
-                                  )
-                                })
-                              }
-                          </div>
-                          </div>
-                        </li>
-                      ))
-                  )
-                } else {
-                  return <TextCTA className="text-deep-green text-base font-inter hover:opacity-55 transition-all active:scale-95" key={item.cta_text} text={item.cta_text} link={item.cta_link} />
-                }
+                      });
 
-              })}
-            </ul>
-          )}
-        </>
-    )
+                      let finalWord;
+
+                      if(upperCaseValue.length > 1) {
+                        finalWord = upperCaseValue.join(" ");
+                      } else {
+                        finalWord = upperCaseValue;
+                      }
+
+                      return(
+                      <li key={index} className="relative">
+                        <div className="drop-down">
+                          <RichText 
+                            text={finalWord}
+                            className="text-deep-green text-base font-inter hover:opacity-55 transition-all active:scale-95 relative cursor-pointer"
+                          />
+                          <div className="py-4 space-y-4 z-20 bg-cream border border-deep-green rounded min-w-[200px] drop-down-container">
+                            {drop_down_items
+                              .filter((data) => {
+                                  const lowerCaseValue = item.toLowerCase();
+                                  const matchData = lowerCaseValue === data.identifier.toLowerCase();
+                                  return matchData;
+                              })
+                              .map((drop_down, i) => {
+                                return(
+                                  <p className="px-4 hover:opacity-55 transition-all active:scale-95" key={i}>
+                                    <PrismicNextLink
+                                      field={drop_down.cta_link}>
+                                      <span className="text-deep-green text-base font-inter">
+                                        {drop_down.cta_text}
+                                      </span>
+                                    </PrismicNextLink>
+                                  </p>
+                                )
+                              })
+                            }
+                        </div>
+                        </div>
+                      </li>
+                      )
+                    })
+                )
+              } else {
+                return <TextCTA className="text-deep-green text-base font-inter hover:opacity-55 transition-all active:scale-95" key={item.cta_text} text={item.cta_text} link={item.cta_link} />
+              }
+
+            })}
+            
+          </ul>
+        )}
+      </>
+  )
 
 }
 
