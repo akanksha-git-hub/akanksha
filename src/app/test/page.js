e0a232b6341b1ac3a5a0c2dea77c796e43ba2ebf
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-11-22 13:40:18 +0530

    testing dummy card anim

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,10 +1,45 @@
+'use client'
 import Dummy from '@/components/dummy';
-export default async function Page() {
+
+export default function Page() {
+
+
+
 
     return(
-        <main className="h-screen flex items-center justify-center">
-            <Dummy 
-            />
+        // <main className='pers'>
+        //     {/* <Dummy 
+        //     /> */}
+        //     <div className='flex flex-col items-center justify-center target border border-red-500'>
+        //         {/* <p className='font-ambit-regular text-white'>
+        //             MAIN SECTION
+        //         </p> */}
+        //         {/* <div className='cube relative h-[250px] w-[250px] border border-white'>
+        //             <div 
+        //                 className='front-side grid place-items-center'
+        //             >
+        //                 FRONT SIDE
+        //             </div>
+        //         </div> */}
+        //         <div className='h-[250px] w-[250px] bg-blue-400 test-B'>
+
+        //         </div>
+        //         <div className='test-z h-[250px] w-[250px] bg-red-500'>
+
+        //         </div>
+        //     </div>
+        //     <div className='h-[200vh]'>
+
+        //     </div>
+        // </main>
+        <main> 
+             <div className='cube relative h-[250px] w-[250px] border border-white'>
+                     <div 
+                        className='front-side grid place-items-center'
+                    >
+                        FRONT SIDE
+                    </div>
+                </div> 
         </main>
     )
 }
