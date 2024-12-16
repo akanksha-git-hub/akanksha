e041e6fcd1486137e6774654a3e09d7572511bc4
Sumesh-gradicals <sumesh@gradical.xyz>
2024-12-16 16:35:30 +0530

    fixed for responsiveness

--- src/components/ProgramShowcase/StatsComponent.jsx
+++ src/components/ProgramShowcase/StatsComponent.jsx
@@ -1,19 +1,19 @@
-import RichText from '../Texts/RichText'
+import RichText from "../Texts/RichText";
 
 export default function StatsComponent({ description, number }) {
   return (
-    <div 
+    <div
       className="rounded-lg p-4 
-      bg-soft-white h-[15.6rem] flex flex-col items-start justify-between w-2/4 sm:w-full"
+      bg-soft-white h-[15.6rem] flex flex-col items-start justify-between w-2/4 sm:w-full  "
     >
-      <RichText 
+      <RichText
         className="font-ambit-regular text-deep-green text-sm w-[90%] xl:w-[70%]"
         text={description}
       />
-      <RichText 
+      <RichText
         className="font-ambit-semibold text-deep-green mt-12 sm:mt-0 text-3xl sm:text-6xl xl:text-8xl"
         text={number}
       />
     </div>
-  )
+  );
 }
