0fadbc675dde5442c085768ab9641c234b104807
Sumesh-gradicals <sumesh@gradical.xyz>
2025-01-06 17:14:00 +0530

    edit impact

--- src/components/v2-components/impact-card-shuffle/CardB.js
+++ src/components/v2-components/impact-card-shuffle/CardB.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function CardB({item}) {
     console.log(item);
     
@@ -21,15 +23,60 @@ export default function CardB({item}) {
             </div>
     
             {/* Section 2 */}
-            <div className="w-full md:w-2/4   border border-red-400">
+            <div className="w-full md:w-2/4 ">
     {/* Rotated Container */}
     <div className=" flex flex-col justify-evenly  w-full h-full ">
         {/* First Chart */}
-        <div className="bg-[#FBDA1D]  w-full"><div className="p-2">Year 2023-24</div></div>
+        <div className="bg-[#FBDA1D] w-full flex items-center justify-between p-4">
+  {/* Text */}
+  <div className="font-ambit-regular text-sm sm:text-lg md:text-xl lg:text-2xl">
+    Year 2023-24
+  </div>
+
+  {/* Image and Percentage Container */}
+  <div className="flex items-center justify-end">
+    {/* Image */}
+    <Image
+      src="/hover-3.png"
+      alt="Hovered Image"
+      width={12} 
+      height={12} 
+    />
+
+    {/* Percentage */}
+    <div className="font-ambit-regular text-xl sm:text-2xl md:text-3xl lg:text-6xl ml-2">
+      99.4%
+    </div>
+  </div>
+</div>
+
+
         {/* Second Chart */}
-        <div className="bg-[#F6AC27]  w-full">Year 2023-24</div>
+        <div className="bg-[#F6AC27] ml-auto w-[60%] flex items-center justify-between p-4">
+  {/* Text */}
+  <div className="font-ambit-regular text-sm sm:text-lg md:text-xl lg:text-2xl">
+    Year 2023-24
+  </div>
+
+  {/* Percentage */}
+  <div className="font-ambit-regular text-xl sm:text-2xl md:text-3xl lg:text-6xl">
+    99.4%
+  </div>
+</div>
+
         {/* Third Chart */}
-        <div className="bg-[#ECF0F1] w-full">Year 2023-24</div>
+        <div className="bg-[#ECF0F1] ml-auto w-[80%] flex items-center justify-between p-4">
+  {/* Text */}
+  <div className="font-ambit-regular text-sm sm:text-lg md:text-xl lg:text-2xl">
+    Year 2023-24
+  </div>
+
+  {/* Percentage */}
+   <div className="font-ambit-regular text-xl sm:text-2xl md:text-3xl lg:text-6xl">
+    99.4%
+  </div>
+</div>
+
     </div>
 </div>
 
