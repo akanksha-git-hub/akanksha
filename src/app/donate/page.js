e76746c824e4c81eab713a33fb5382a6024f3f51
Sumesh-gradicals <sumesh@gradical.xyz>
2025-01-24 17:04:57 +0530

    donate page padding

--- src/app/donate/page.js
+++ src/app/donate/page.js
@@ -4,28 +4,27 @@ import { maxwidth } from "@/utils/helperClasses";
 import { createClient } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
-
 export default async function Page() {
+  const page = await fetchPrismicSingleDocument("donor_page");
+  const context = { addPadding: true };
 
-    const page = await fetchPrismicSingleDocument("donor_page");
+  if (!page) return null;
 
-    if(!page) return null;
-
-    return(
-        <main className={`${maxwidth}`}>
-            <SliceZone 
-                components={components}
-                slices={page.data.slices}
-            />
-        </main>
-    )
-
+  return (
+    <main className={`${maxwidth}`}>
+      <SliceZone
+        components={components}
+        slices={page.data.slices}
+        context={context}
+      />
+    </main>
+  );
 }
 
 // export async function generateMetadata() {
 //     const client = createClient();
 //     const page = await client.getSingle("donor_page");
-  
+
 //     return {
 //       title: page.data.meta_title,
 //       description: page.data.meta_description,
