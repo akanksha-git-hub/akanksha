bbaa19f128202ad8a7ea4e9819ad312f57dbae0c
Sumesh-gradicals <sumesh@gradical.xyz>
2025-01-29 12:17:18 +0530

    removed gradiant from core values

--- src/slices/PageTitle/index.js
+++ src/slices/PageTitle/index.js
@@ -14,76 +14,61 @@ import SparkleMedium from "@/components/Sparkles/sparkle-medium";
  * @param {PageTitleProps}
  */
 const PageTitle = ({ slice }) => {
-
-
-  if(slice.variation === 'optionC') {
-
-    return(
+  if (slice.variation === "optionC") {
+    return (
       <section
         data-slice-type={slice.slice_type}
         data-slice-variation={slice.variation}
         className="my-12 space-y-16"
       >
-        <SliceIdentifier 
-          text={slice.primary.slice_identifier}
-        />
+        <SliceIdentifier text={slice.primary.slice_identifier} />
         <div className="space-y-6">
-          <MixedText 
+          <MixedText
             index={5}
             texts={slice.primary.title}
-            className='font-ambit-regular text-deep-green text-5xl text-left md:text-center max-w-[40ch] mx-auto md:justify-center'
-            spanPosition='top-2 sm:top-[6px]'
+            className="font-ambit-regular text-deep-green text-5xl text-left md:text-center max-w-[40ch] mx-auto md:justify-center"
+            spanPosition="top-2 sm:top-[6px]"
           />
-          <RichText 
+          <RichText
             text={slice.primary.description}
-            className='font-ambit-regular text-deep-green text-lg md:text-center max-w-[80ch] mx-auto'
+            className="font-ambit-regular text-deep-green text-lg md:text-center max-w-[80ch] mx-auto"
           />
         </div>
       </section>
-    )
-
+    );
   }
-  if(slice.variation === 'optionD') {
-
-    return(
+  if (slice.variation === "optionD") {
+    return (
       <section
         data-slice-type={slice.slice_type}
         data-slice-variation={slice.variation}
         className="my-12 space-y-16"
       >
-        <SliceIdentifier 
-          text={slice.primary.slice_identifier}
-        />
+        <SliceIdentifier text={slice.primary.slice_identifier} />
         <div className="space-y-6">
-          <MixedText 
-          
+          <MixedText
             texts={slice.primary.title}
-            className='font-ambit-regular text-black text-5xl md:text-7xl text-left md:text-center max-w-[15ch]  md:justify-start'
-            
+            className="font-ambit-regular text-black text-5xl md:text-7xl text-left md:text-center max-w-[15ch]  md:justify-start"
           />
-          <RichText 
+          <RichText
             text={slice.primary.description}
-            className='font-ambit-regular text-black text-lg     '
+            className="font-ambit-regular text-black text-lg     "
           />
         </div>
       </section>
-    )
-
+    );
   }
   return (
     <>
-    <div className="universal-padding">
-    <SliceIdentifier text={slice.primary.slice_identifier}  />
-    </div>
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      className={`flex flex-col items-center ${slice.variation === 'default' && ('universal-padding mt-24')} pt-28`}
-
-    >
-      
-       
-      {/* {slice.variation === 'sparkle' && (
+      <div className="universal-padding">
+        <SliceIdentifier text={slice.primary.slice_identifier} />
+      </div>
+      <section
+        data-slice-type={slice.slice_type}
+        data-slice-variation={slice.variation}
+        className={`flex flex-col items-center ${slice.variation === "default" && "universal-padding mt-24"} pt-28`}
+      >
+        {/* {slice.variation === 'sparkle' && (
         <div 
           className="flex items-center justify-center w-full md:w-[50vw] max-w-[650px] relative left-0 md:left-16"
         >
@@ -95,14 +80,15 @@ const PageTitle = ({ slice }) => {
           />
         </div>
       )} */}
-      <div className={`flex flex-col items-center ${slice.variation === 'sparkle' && ('universal-padding')} !pt-0 space-y-8`}>
-        <div className="space-y-2 w-full flex flex-col md:items-center relative">
-          
-          <RichText 
-            text={slice.primary.title}
-            className={`text-deep-green font-ambit-regular text-7xl ${slice.variation === 'sparkle' ? 'text-left' : 'text-center'} md:text-center w-full sm:w-[70%]`}
-          />
-          {/* <SparkleMedium 
+        <div
+          className={`flex flex-col items-center ${slice.variation === "sparkle" && "universal-padding"} !pt-0 space-y-8`}
+        >
+          <div className="space-y-2 w-full flex flex-col md:items-center relative">
+            <RichText
+              text={slice.primary.title}
+              className={`text-deep-green font-ambit-regular text-7xl ${slice.variation === "sparkle" ? "text-left" : "text-center"} md:text-center w-full sm:w-[70%]`}
+            />
+            {/* <SparkleMedium 
             className="absolute -z-10 -top-[70%] md:-top-32 -left-1/4 w-full h-full scale-[0.2] md:scale-[0.4]"
           />
           <SparkleMedium 
@@ -111,24 +97,17 @@ const PageTitle = ({ slice }) => {
           <SparkleMedium 
             className="absolute -z-10 top-0 -right-[20%] w-full h-full scale-[0.2]"
           /> */}
-          {/* TODO Change Heart Image to prismic later */}
-          <Image 
-            src={PinkHeart}
-            height={100}
-            width={100}
-            alt="heart"
-          />
+            {/* TODO Change Heart Image to prismic later */}
+            <Image src={PinkHeart} height={100} width={100} alt="heart" />
+          </div>
+          <div
+            className={`relative text-deep-green font-ambit-regular text-lg text-left md:text-center ${slice.variation === "default" ? "w-full lg:w-[70%] xl:w-[70ch]" : "w-full lg:w-[80%] 3xl:w-[110ch]"}`}
+          >
+            {/* <div className="orange-gradient absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 -z-20 h-full w-full"></div> */}
+            <PrismicRichText field={slice.primary.description} />
+          </div>
         </div>
-        <div
-          className={`relative text-deep-green font-ambit-regular text-lg text-left md:text-center ${slice.variation === 'default' ? 'w-full lg:w-[70%] xl:w-[70ch]' : 'w-full lg:w-[80%] 3xl:w-[110ch]'}`}
-        >
-          <div className="orange-gradient absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 -z-20 h-full w-full"></div>
-          <PrismicRichText 
-            field={slice.primary.description}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
