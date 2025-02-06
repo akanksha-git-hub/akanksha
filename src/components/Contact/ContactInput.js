3149ac56fe5222f50da5f120c2a30a7bf95e3095
Sumesh-gradicals <sumesh@gradical.xyz>
2025-02-06 05:47:51 +0530

    contact form

--- src/components/Contact/ContactInput.js
+++ src/components/Contact/ContactInput.js
@@ -2,19 +2,19 @@
 export default function ContactInput({label, variant="default", className, ...props}) {
   return (
     <p className={`flex flex-col input-parent-state ${className}`}>
-        <label className="contact-label text-xl font-ambit-regular text-deep-green label-state">
+        <label className="contact-label text-xl font-ambit-regular text-black label-state">
             {label}
         </label>
         {
             variant === "message" ?
             <textarea 
                 {...props}
-                className={`outline-none border-0 border-b py-2 font-ambit-regular text-deep-green text-lg contact-input-state`}
+                className={`outline-none border-0 border-b py-2 font-ambit-regular text-black text-lg contact-input-state`}
             />
             :
             <input
                 {...props} 
-                className={`outline-none border-0 border-b py-2 font-ambit-regular text-deep-green text-lg contact-input-state`}
+                className={`outline-none border-0 border-b py-2 font-ambit-regular text-black text-lg contact-input-state`}
             /> 
         }
     </p>
