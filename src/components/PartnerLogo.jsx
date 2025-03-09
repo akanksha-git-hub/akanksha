3d791803cae1d1f9f2fa835d99c7d3354991fa07
Sumesh-gradicals <sumesh@gradical.xyz>
2025-03-09 10:42:50 +0530

    marquee fix-18

--- src/components/PartnerLogo.jsx
+++ src/components/PartnerLogo.jsx
@@ -3,7 +3,7 @@ import { PrismicNextImage } from "@prismicio/next";
 export default function PartnerLogo({ image, className, imageClassName }) {
   return (
     <li className={`${className}`}>
-      <PrismicNextImage height={600} alt="" width={600} className={`${imageClassName}`} field={image} />
+      <PrismicNextImage height={100} alt="" width={100} className={`${imageClassName}`} field={image} />
     </li>
   )
 }
