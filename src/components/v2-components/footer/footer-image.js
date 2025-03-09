d0a45dd7623b1a064ed15dc317ba123e87a8aae8
Sumesh-gradicals <sumesh@gradical.xyz>
2025-03-09 14:48:32 +0530

    footer mobile fix

--- src/components/v2-components/footer/footer-image.js
+++ src/components/v2-components/footer/footer-image.js
@@ -4,7 +4,7 @@ import CloudWall from "@/assets/cloud-wall.svg";
 export default function FooterImage() {
   return (
     <>
-      <div className="absolute -right-12 top-2/4 -translate-y-2/4 h-[50rem] sm:w-[24vw] w-[34vw] ">
+      <div className="absolute -right-12  top-2/4 -translate-y-1/4 md:-translate-y-2/4 h-[50rem] sm:w-[24vw] w-[44vw] ">
         <div className="relative h-full w-full">
           <Image src={CloudWall} alt="image" fill />
         </div>
