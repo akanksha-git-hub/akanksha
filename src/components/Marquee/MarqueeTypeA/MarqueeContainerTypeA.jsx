0554e845effdd386d0df0157e6ca35fe0c6f3a5e
Sumesh-gradicals <sumesh@gradical.xyz>
2025-03-09 09:55:25 +0530

    marquee fix-12

--- src/components/Marquee/MarqueeTypeA/MarqueeContainerTypeA.jsx
+++ src/components/Marquee/MarqueeTypeA/MarqueeContainerTypeA.jsx
@@ -6,26 +6,26 @@ export default function MarqueeContainerTypeA({ items, direction }) {
         <div className="overflow-hidden py-2 whitespace-nowrap">
             <div className="marquee-container">
                 <div className="marquee-items">
-                <div className={`marquee-items-slide ${isRight ? 'marquee-items-slide-right' : 'marquee-items-slide-left'}`}>
+                    <div className={`marquee-items-slide ${direction}`}>
                         {items.map((logo, index) => {
                             return(
                                 <PartnerLogo 
                                     image={logo.partner_logo}
                                     key={index}
                                     imageClassName="h-[70%] w-[70%] object-contain"
-                                    className="flex items-center justify-center border border-[#DCDCDC] mb-0 h-[7rem] w-[30%]"              
+                                    className="flex items-center justify-center border border-[#DCDCDC] mb-0 h-[8rem] w-[30%]"              
                                 />
                             )
                         })}
                     </div>
-                    <div className={`marquee-items-slide ${isRight ? 'marquee-items-slide-right' : 'marquee-items-slide-left'}`}>
+                    <div className={`marquee-items-slide ${direction}`}>
                         {items.map((logo, index) => {
                             return(
                                 <PartnerLogo 
                                     image={logo.partner_logo}
                                     key={index}
                                     imageClassName="h-[70%] w-[70%] object-contain"
-                                    className="flex items-center justify-center border border-[#DCDCDC] mb-0 h-[7rem] w-[30%]"              
+                                    className="flex items-center justify-center border border-[#DCDCDC] mb-0 h-[8rem]w-[30%]"              
                                 />
                             )
                         })}
