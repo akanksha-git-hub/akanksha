7d8166cbc97acead1da541bc91cfb89d55a54f46
Sumeshpad <sumesh@gradical.xyz>
2025-03-14 17:49:32 +0530

    films page

--- src/app/resources/films/page.js
+++ src/app/resources/films/page.js
@@ -5,52 +5,43 @@ import { maxwidth } from "@/utils/helperClasses";
 import { PrismicNextImage } from "@prismicio/next";
 import { SliceZone } from "@prismicio/react";
 
-
 export default async function Page() {
+  const page = await fetchPrismicSingleDocument("films");
 
-    const page = await fetchPrismicSingleDocument('films');
+  if (!page) return <p>No page data</p>;
 
-    if(!page) return <p>No page data</p>;
-    
-
-
-    return(
-        <main className={`${maxwidth} universal-padding mt-28 `}>
-        <div className="flex flex-row justify-center items-center gap-2 ">
-            {/* Left Image */}
-            <PrismicNextImage 
-                field={page.data.left_image} 
-                alt="Left Image"
-                className="hidden md:block md:w-60 -translate-y-12" // Adjust size as needed
-            />
-    
-            {/* Text Container - Column Layout */}
-            <div className="flex flex-col items-center text-black">
-                <RichText 
-                    text={page.data.title}
-                    className="font-ambit-regular text-7xl md:text-center"
-                />
-                <RichText 
-                    text={page.data.sub_title}
-                    className="text-2xl font-playfair-display md:text-center"
-                />
-            </div>
-    
-            {/* Right Image */}
-            <PrismicNextImage 
-                field={page.data.right_image} 
-                alt="Right Image"
-                className="hidden md:block md:w-40 -translate-y-12 " 
-            />
-        </div>
-    
-        {/* SliceZone */}
-        <SliceZone 
-            components={components}
-            slices={page.data.slices}
+  return (
+    <main className={`${maxwidth} universal-padding mt-14 `}>
+      <div className="flex flex-row justify-center items-center gap-2 ">
+        {/* Left Image */}
+        <PrismicNextImage
+          field={page.data.left_image}
+          alt="Left Image"
+          className="hidden md:block md:w-60 -translate-y-12" // Adjust size as needed
         />
-    </main>
-    
-    )
 
+        {/* Text Container - Column Layout */}
+        <div className="flex flex-col items-center text-black">
+          <RichText
+            text={page.data.title}
+            className="font-ambit-regular text-5xl md:text-7xl md:text-center"
+          />
+          <RichText
+            text={page.data.sub_title}
+            className="text-2xl font-ambit-regular md:text-center"
+          />
+        </div>
+
+        {/* Right Image */}
+        <PrismicNextImage
+          field={page.data.right_image}
+          alt="Right Image"
+          className="hidden md:block md:w-36 -translate-y-8 "
+        />
+      </div>
+
+      {/* SliceZone */}
+      <SliceZone components={components} slices={page.data.slices} />
+    </main>
+  );
 }
--- src/components/Resources/ResourcesCardPaginationContainer.jsx
+++ src/components/Resources/ResourcesCardPaginationContainer.jsx
@@ -1,36 +1,36 @@
 import { useSmoothScroller } from "../LenisScrollContext";
 import CTA from "../UI/Button/CTA";
-import { useResourcesCardContext } from "./ResourcesCard"
+import { useResourcesCardContext } from "./ResourcesCard";
 
 export default function ResourcesCardPaginationContainer() {
+  const {
+    incrementPagination,
+    decrementPagination,
+    state: { totalLength, initial, final },
+  } = useResourcesCardContext();
 
-  const 
-    { 
-      incrementPagination, 
-      decrementPagination, 
-      state: { totalLength, initial, final } 
-    } = useResourcesCardContext();
+  const { paginationScrollTo } = useSmoothScroller();
 
-    const { paginationScrollTo } = useSmoothScroller();
+  function incrementFunction() {
+    paginationScrollTo();
+    incrementPagination(totalLength);
+  }
 
-    function incrementFunction() {
-      paginationScrollTo();
-      incrementPagination(totalLength);
-    }
-
-    function decrementFunction() {
-      paginationScrollTo();
-      decrementPagination();
-    }
+  function decrementFunction() {
+    paginationScrollTo();
+    decrementPagination();
+  }
 
   return (
     <>
-    {totalLength >= 6 && (
-      <div className="flex items-center justify-center gap-4">
-        {initial !== 0 && (<CTA onClick={decrementFunction} text='Prev' />)}
-        {final !== totalLength && (<CTA onClick={incrementFunction} text='Next' />)}
-      </div>
-    )}
+      {totalLength >= 6 && (
+        <div className="flex items-center justify-center gap-4 ">
+          {initial !== 0 && <CTA onClick={decrementFunction} text="Prev" />}
+          {final !== totalLength && (
+            <CTA onClick={incrementFunction} text="Next" />
+          )}
+        </div>
+      )}
     </>
-  )
+  );
 }
