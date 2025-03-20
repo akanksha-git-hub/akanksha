5102828f2a0e93a29f482b709855c10db2cd4aaa
Sumesh-gradicals <sumesh@gradical.xyz>
2025-03-20 18:17:47 +0530

    donor page

---
+++ src/components/v2-components/CardWithHeadingStatIcon.jsx
@@ -0,0 +1,26 @@
+import { PrismicNextImage } from '@prismicio/next';
+
+export default function CardWithHeadingStatIcon({ heading, stat, iconField }) {
+  return (
+    <div className="flex flex-col items-start justify-between border-[0.15rem]  border-black md:hover:bg-v2-yellow p-6 md:w-[50%] w-full mt-20 gap-4 ">
+      <h2 className="font-ambit-regular text-black text-4xl md:text-5xl">
+        {heading}
+      </h2>
+      <p className="font-ambit-regular text-black text-xl md:text-2xl ">
+        {stat}
+      </p>
+      <div className="relative w-10 h-10 md:w-14 md:h-14">
+        {iconField?.url ? (
+          <PrismicNextImage
+            field={iconField}
+            fill
+            style={{ objectFit: 'contain' }}
+            alt={iconField?.alt || 'Icon'}
+          />
+        ) : (
+          <div className="h-full w-full bg-black rounded-full" />
+        )}
+      </div>
+    </div>
+  );
+}
