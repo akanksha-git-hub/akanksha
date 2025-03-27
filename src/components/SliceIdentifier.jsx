57c28ce53215efd434a3727b2e313de0104af5e5
Sumeshpad <sumesh@gradical.xyz>
2025-03-27 12:25:51 +0530

    slice Identifier used css instead of gsap

--- src/components/SliceIdentifier.jsx
+++ src/components/SliceIdentifier.jsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useRef, useEffect } from "react";
 import { PrismicNextImage } from "@prismicio/next";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
+import { useEffect, useRef, useState } from "react";
 
 export default function SliceIdentifier({
   text = "",
@@ -13,48 +9,28 @@ export default function SliceIdentifier({
   hasSpider,
   isVisible,
 }) {
+  const safeText = text ?? "";
   const containerRef = useRef(null);
-  const lineRef = useRef(null);
-  const lettersRef = useRef([]);
+  const [inView, setInView] = useState(false);
 
   useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) setInView(true);
+      },
+      { threshold: 0.5, rootMargin: "0px 0px -30% 0px" }
+    );
+
     if (containerRef.current) {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "top 80%",
-          toggleActions: "play none none none",
-        },
-      });
-
-      // Animate underline
-      tl.fromTo(
-        lineRef.current,
-        { scaleX: 0, transformOrigin: "left" },
-        {
-          scaleX: 1,
-          duration: 1,
-          ease: "power2.out",
-        }
-      );
-
-      // Animate letters (sliding in from left)
-      tl.fromTo(
-        lettersRef.current,
-        { x: -20, opacity: 0 },
-        {
-          x: 0,
-          opacity: 1,
-          stagger: 0.05,
-          ease: "power2.out",
-          duration: 0.6,
-        },
-        "<" // start at the same time as underline
-      );
+      observer.observe(containerRef.current);
     }
-  }, []);
 
-  const safeText = text ?? "";
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div ref={containerRef} className="relative">
@@ -66,18 +42,23 @@ export default function SliceIdentifier({
         {safeText.split("").map((letter, index) => (
           <span
             key={index}
-            ref={(el) => (lettersRef.current[index] = el)}
-            className={`inline-block ${
-              letter === " " ? "w-2 inline-block" : ""
-            }`}
+            className={`inline-block transition-all duration-500 ease-out ${
+              inView
+                ? `animate-slideIn opacity-100 delay-[${index * 40}ms]`
+                : "opacity-0 -translate-x-5"
+            } ${letter === " " ? "w-2 inline-block" : ""}`}
           >
             {letter === " " ? "\u00A0" : letter}
           </span>
         ))}
       </div>
 
-      {/* Bottom Border (animated) */}
-      <div ref={lineRef} className="border-b border-black w-full scale-x-0" />
+      {/* Bottom Border (underline) */}
+      <div
+        className={`border-b border-black w-full transform transition-transform duration-700 ease-out origin-left ${
+          inView ? "scale-x-100" : "scale-x-0"
+        }`}
+      />
 
       {/* Optional Spider Image */}
       {isVisible && (
