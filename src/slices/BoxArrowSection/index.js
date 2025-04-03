92b6d9456e4ae5fa84dc80b7b01e13dd8a7a695a
Sumesh-gradicals <sumesh@gradical.xyz>
2025-04-03 12:21:03 +0530

    box responsiveness

--- src/slices/BoxArrowSection/index.js
+++ src/slices/BoxArrowSection/index.js
@@ -22,11 +22,11 @@ const BoxArrowSection = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="w-full mt-32"
+      className="w-full mt-12 xl:mt-32"
     >
       <div className="max-w-7xl mx-auto px-4">
         {/* Desktop View */}
-        <div className="hidden lg:block ">
+        <div className="hidden xl:block ">
           {boxRows.map((row, rowIndex) => (
             <div key={rowIndex} className="flex items-center justify-center gap-6 mb-6 ">
               {row.map((item, itemIndex) => (
@@ -66,18 +66,44 @@ const BoxArrowSection = ({ slice }) => {
         </div>
 
         {/* Mobile View */}
-        <div className="grid grid-cols-1 gap-6 lg:hidden">
-          {slice.primary.box.map((item, index) => (
-            <div
-              key={index}
-              className="relative text-black p-6 flex flex-col justify-between w-full min-h-[320px]"
-              style={{ backgroundColor: item.hex_color }}
-            >
-              <h2 className="text-5xl font-ambit-regular mb-4">{item.heading}</h2>
-              <p className="text-lg font-ambit-regular">{item.description}</p>
-            </div>
-          ))}
+        <div className="grid grid-cols-1 gap-8 md:gap-16 xl:hidden">
+  {slice.primary.box.map((item, index) => (
+    <div key={index} className="flex flex-col items-center">
+      {/* Card Box */}
+      <div
+        className="relative text-black p-10 flex flex-col justify-between w-full min-h-[320px]"
+        style={{ backgroundColor: item.hex_color }}
+      >
+        <h2 className="text-5xl font-ambit-regular mb-4">{item.heading}</h2>
+        <p className="text-lg md:text-3xl font-ambit-regular w-[80%]">{item.description}</p>
+
+        <div className="absolute top-1/2 -translate-y-1/2 left-0 z-10">
+          <Image
+            src="/quote-left.png"
+            alt="quote icon"
+            width={14}
+            height={60}
+            className="object-cover h-auto"
+          />
         </div>
+      </div>
+
+
+      {index !== slice.primary.box.length - 1 && (
+        <Image
+          src="/arrow.png"
+          alt="arrow"
+          width={44}
+          height={44}
+          className=" block md:hidden mt-10 transform rotate-90"
+        />
+      )}
+    </div>
+  ))}
+</div>
+
+
+       
       </div>
     </section>
   );
