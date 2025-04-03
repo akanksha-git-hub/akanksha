6b87c320fa585929d4b9f6097686d14a2307dc94
Sumesh-gradicals <sumesh@gradical.xyz>
2025-04-03 13:41:26 +0530

    impact grid final

--- src/slices/ImpactMarquee/index.js
+++ src/slices/ImpactMarquee/index.js
@@ -13,11 +13,11 @@ const ImpactMarquee = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="flex  lg:flex-row flex-col lg:space-x-4 justify-between  items-stretch mt-4  min-h-[250px]    ">
-      <div className="flex flex-col  p-8 lg:w-[55%] w-full relative border-[0.15rem] border-black md:hover:bg-v2-yellow mt-4 lg:mt-0 space-y-4 ">
+      <div className="flex  xl:flex-row flex-col xl:space-x-4 justify-between  items-stretch mt-4  min-h-[250px]    ">
+      <div className="flex flex-col  p-8 xl:w-[55%] w-full relative border-[0.15rem] border-black md:hover:bg-v2-yellow mt-4 lg:mt-0 space-y-4 ">
   {/* Top Section: Card Heading and Description */}
-  <div className="flex flex-col justify-between items-center h-full">
-  <div className="flex flex-row items-start  min-h-[180px] ">
+  <div className="flex flex-col justify-between items-center h-full  overflow-hidden">
+  <div className="flex flex-col md:flex-row items-start  min-h-[180px] ">
     <h1 className="font-ambit-regular text-5xl text-left">
       {slice.primary.card_1_heading}
     </h1>
@@ -27,32 +27,30 @@ const ImpactMarquee = ({ slice }) => {
   </div>
 
   {/* Bottom Section: Marquee */}
-  <div className="flex flex-wrap justify-around gap-4 mt-4">
+  <div className="flex flex-wrap justify-around gap-4 mt-4  ">
     <MarqueeNew data={data} />
   </div>
   </div>
 </div>
 
-<div className="flex flex-col p-8 lg:w-[45%] w-full relative border-[0.15rem] border-black md:hover:bg-v2-yellow mt-4 lg:mt-0 space-y-4">
+<div className="flex flex-col p-8 xl:w-[45%] w-full relative border-[0.15rem] border-black md:hover:bg-v2-yellow mt-4 xl:mt-0 space-y-4">
   
   
-<div className="flex flex-col justify-around items-start h-full w-full" >
-  <div className="relative w-full h-14 md:w-full md:h-44 ">
-              <PrismicNextImage
-                field={slice.primary.card_2_image}
-                fill
-                style={{ objectFit: 'contain' }}
-                alt={slice.primary.card_2_image?.alt || 'Icon'}
-              />
-            </div>
-            <div>
-<p className="font-ambit-regular text-black text-[1.35rem] lg:text-left">
-  {slice.primary.card_2_description}
-</p>
-            </div>
+<div className="flex flex-col justify-start items-start h-full w-full ">
+  {/* Image container aligned to left */}
+  <PrismicNextImage
+                                field={slice.primary.card_2_image}
+                                className=" h-full w-full object-cover"
+                            />
 
-
+  {/* Paragraph aligned to left edge */}
+  <div className="w-full  mt-4 ">
+    <p className="font-ambit-regular text-black  text-lg md:text-[1.35rem] text-left">
+      {slice.primary.card_2_description}
+    </p>
+  </div>
 </div>
+
   
               
                   
