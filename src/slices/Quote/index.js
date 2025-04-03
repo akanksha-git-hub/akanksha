85dbdf54c9a24516245a57c0cee18d9efb8197c9
Sumesh-gradicals <sumesh@gradical.xyz>
2025-04-03 14:57:26 +0530

    quote responsiceness

--- src/slices/Quote/index.js
+++ src/slices/Quote/index.js
@@ -8,35 +8,36 @@ import { PrismicImage } from "@prismicio/react";
 const Quote = ({ slice }) => {
   return (
     <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    className="uiversal-padding mt-16 ">
- <div className="relative w-full max-w-3xl h-64 mx-auto ">
+    data-slice-type={slice.slice_type}
+    data-slice-variation={slice.variation}
+    className="uiversal-padding mt-20"
+  >
+    <div className="relative w-full max-w-3xl  lg:min-h-[250px] mx-auto  bg-white p-6 sm:p-10 flex items-center justify-center text-center">
+      
+      {/* Top quote icon */}
+      <div className="absolute top-2 left-2 sm:top-4 sm:left-4 w-6 h-6 sm:w-12 sm:h-12">
+        <PrismicImage
+          field={slice.primary.quote_image}
+          className="w-full h-full object-contain"
+        />
+      </div>
   
-  <div className="absolute top-4 left-4 w-12 h-12">
-    <PrismicImage
-      field={slice.primary.quote_image}
-      className="w-full h-full object-contain"
-    />
-  </div>
-
+      {/* Centered Text */}
+      <p className="text-[1.1rem] sm:text-[1.35rem] font-ambit-regular px-4 sm:px-6">
+        {slice.primary.quote}
+      </p>
   
-  <div className="flex items-center justify-center h-full px-6 text-center">
-    <p className="text-[1.35rem] font-ambit-regular">
-      {slice.primary.quote}
-    </p>
-  </div>
-
+      {/* Bottom quote icon */}
+      <div className="absolute bottom-2 right-2 sm:bottom-4 sm:right-4 w-6 h-6 sm:w-12 sm:h-12 transform scale-y-[-1] scale-x-[-1]">
+        <PrismicImage
+          field={slice.primary.quote_image}
+          className="w-full h-full object-contain"
+        />
+      </div>
+    </div>
+  </section>
   
-  <div className="absolute bottom-4 right-4 w-12 h-12 transform scale-y-[-1] scale-x-[-1]">
-    <PrismicImage
-      field={slice.primary.quote_image}
-      className="w-full h-full object-contain"
-    />
-  </div>
-</div>
-
-    </section>
+  
   );
 };
 
