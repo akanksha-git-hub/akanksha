fb5ee769760a0ede2cc41bdd1d787eb68c39d1f4
Sumesh-gradicals <sumesh@gradical.xyz>
2025-05-23 16:12:05 +0530

    billdesk - 33

--- src/app/api/create-order-billdesk/route.js
+++ src/app/api/create-order-billdesk/route.js
@@ -52,7 +52,7 @@ export async function POST(req) {
       amount: amount.toString(),
       order_date: orderDate,
       currency: '356',
-      ru: `${process.env.APP_BASE_URL}/api/billdesk-payment-return`,
+      ru: 'https://dev.akanksha.org/thank-you',
       additional_info: {
         additional_info1: stepB?.donate_to || 'General Donation',
         additional_info2: stepB?.heard_from || 'Website',
