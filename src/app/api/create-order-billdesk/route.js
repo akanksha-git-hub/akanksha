0979c410f81fe8b5f1f5412d0dcb5484d0a45472
Sumesh-gradicals <sumesh@gradical.xyz>
2025-06-13 17:01:02 +0530

    changed from max to fixed

--- src/app/api/create-order-billdesk/route.js
+++ src/app/api/create-order-billdesk/route.js
@@ -98,7 +98,7 @@ mandate_required: type ? undefined : 'Y',
     start_date: new Date().toISOString().split('T')[0],
     end_date: "2030-12-31",
     frequency: "mnth",
-    amount_type: "maximum",
+    amount_type: "fixed",
     debit_day: "1",
     mandate_auth_mode: "AADHAAR_OTP",
     
