694ec9f0cc2e7aa3d6383af25478013f95e6cfe2
Sumesh-gradicals <sumesh@gradical.xyz>
2025-07-18 01:47:24 +0530

    terms link final

--- src/prismicio.js
+++ src/prismicio.js
@@ -112,12 +112,7 @@ const routes = [
     type: "privacy_policy",
     path: "/privacy-policy",
   },
- {
-  type: "terms_and_conditions",
-  path: "/terms-and-conditions",
-},
-
-
+  
   {
     type: "impact",
     path: "/our-impact",
