d076e9a4136405ac9a1fc542c37b2c4a117782be
Sumesh-gradicals <sumesh@gradical.xyz>
2025-08-08 10:58:42 +0530

    min payemet 1

--- src/components/DonationSelectors.js
+++ src/components/DonationSelectors.js
@@ -33,7 +33,7 @@ export default function DonationSelectors({ data }) {
   const isDisabled =
     active.amountSelector.amount === null ||
    active.amountSelector.amount === 0 ||
-     active.amountSelector.amount < 500;
+     active.amountSelector.amount < 1;
 
   useEffect(() => {
     if (typeof window === "undefined") return;
