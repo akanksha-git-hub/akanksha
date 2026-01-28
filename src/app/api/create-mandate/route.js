82a30fbd6f6e225d663d93f6413ee0551faaa0b2
Sumesh-gradicals <sumesh@gradical.xyz>
2026-01-29 02:51:41 +0530

    removed recurrence type and .

--- src/app/api/create-mandate/route.js
+++ src/app/api/create-mandate/route.js
@@ -119,14 +119,13 @@ const order_date = getISTDateTime(0, 0, true);
     const jwsPayloadObject = {
       mercid: MERC_ID,
       orderid: orderId,
-   
+      order_date,
       customer_refid: customerRefid,
       subscription_refid: subscriptionRefid,
       subscription_desc: "Akanksha Mandate",
       frequency: "adho",
       amount_type: "max",
-      recurrence_rule: "after",
-      debit_day: "6",
+   
       start_date,
       end_date,
       currency: "356",
